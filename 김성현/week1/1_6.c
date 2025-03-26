676be9a09f13bc164c252839fad4f2888f9346d0
tjdgus0231 <andrew02031@gmail.com>
2025-03-26 13:27:47 +0900

    week1_6 수정

--- 김성현/week1/1_6.c
+++ 김성현/week1/1_6.c
@@ -6,16 +6,9 @@ int main(void)
     scanf("%d", &a);
     scanf("%d", &b);
 
-
-    int answer1 = a * (b % 10);
-    printf("%d\n", answer1);
-
-    int answer2 = a * ((b % 100) / 10);
-    printf("%d\n", answer2);
-
-    int answer3 = a * (b / 100);
-    printf("%d\n", answer3);
-
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * ((b % 100) / 10));
+    printf("%d\n", a * (b / 100));
     printf("%d\n", a * b);
 
 
