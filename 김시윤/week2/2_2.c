a7ba941bf84e87c2f7599ca940062df124004752
Xiyoon123 <jbluesea22@gmail.com>
2025-03-31 00:28:40 +0900

    변경

--- 김시윤/week2/2_2.c
+++ 김시윤/week2/2_2.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    int h,m;
-    if (0<=h<=23 && 0<=m<=59){
-        
-
+int main() {
+    int H, M;
+    scanf("%d %d", &H, &M);
+    M -= 45;
+    if (M < 0) {
+        M += 60; 
+        H -= 1;  
+        if (H < 0) {
+            H = 23;
+        }
     }
+    printf("%d %d\n", H, M);
     return 0;
 }
