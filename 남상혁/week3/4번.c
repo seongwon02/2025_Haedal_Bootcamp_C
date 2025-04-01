d30e69a166d4bc91ea9b73deefc3ae9f208450c9
hyuck121212 <hyuck121212poppoppoppop@gmail.com>
2025-04-01 19:35:51 +0900

    Add files via upload

---
+++ 남상혁/week3/4번.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main(){
+    int T, R, i, j, k;
+    char S[20];
+
+    scanf("%d", &T);
+ 
+    for(i=0; i<T; i++){
+        scanf("%d %s", &R, S);
+ 
+        for(j=0; j<strlen(S); j++){
+            for(k=0; k<R; k++){
+                printf("%c", S[j]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
